d430cce85da4df29a418bc76464fccfbdeb43d2c
alpaca-tc <alpaca-tc@alpaca.tc>
2018-03-16 18:36:59 +0900

    Initialize repository

---
+++ lib/activerecord/encryption.rb
@@ -0,0 +1,7 @@
+require "activerecord/encryption/version"
+
+module Activerecord
+  module Encryption
+    # Your code goes here...
+  end
+end
---
+++ spec/activerecord/encryption_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Activerecord::Encryption do
+  it "has a version number" do
+    expect(Activerecord::Encryption::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
