15b88724fef982cfc9000c97568e2514873b117f
alpaca-tc <alpaca-tc@alpaca.tc>
2018-03-27 13:06:52 +0900

    Support Rails5.0 / Rails5.1

--- lib/activerecord_encryption/serializer_with_cast.rb
+++ lib/activerecord_encryption/serializer_with_cast.rb
@@ -26,5 +26,14 @@ module ActiverecordEncryption
         cast(value)
       end
     end
+
+    # Backport
+    if ActiveRecord.gem_version < Gem::Version.create('5.2.0')
+      refine ActiveModel::Type::DateTime do
+        def serialize(value)
+          super(cast(value))
+        end
+      end
+    end
   end
 end
