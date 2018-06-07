443b83c48b8e481f2a7feb7838acfbc4d95ceef0
alpaca-tc <alpaca-tc@alpaca.tc>
2018-06-08 00:39:11 +0900

    Set default encryption instead of cipher

--- lib/active_record_encryption.rb
+++ lib/active_record_encryption.rb
@@ -8,19 +8,12 @@ module ActiveRecordEncryption
   require 'active_record_encryption/serializer_with_cast'
   require 'active_record_encryption/type'
   require 'active_record_encryption/encryptor'
-  require 'active_record_encryption/encryptor/cipher'
   require 'active_record_encryption/encrypted_attribute'
   require 'active_record_encryption/quoter'
   require 'active_record_encryption/exceptions'
 
-  thread_mattr_accessor(:cipher)
-
-  def self.with_cipher(new_cipher)
-    previous = cipher
-    self.cipher = new_cipher
-    yield
-  ensure
-    self.cipher = previous
+  mattr_accessor(:default_encryption, instance_accessor: false) do
+    { encryptor: :raw }
   end
 end
 
--- spec/active_record_encryption_spec.rb
+++ spec/active_record_encryption_spec.rb
@@ -5,21 +5,19 @@ RSpec.describe ActiveRecordEncryption do
     expect(ActiveRecordEncryption::VERSION).not_to be nil
   end
 
-  describe 'with_cipher' do
-    before do
-      described_class.cipher = nil
-    end
-
-    it 'assigns cipher in the block' do
-      cipher = build_cipher
-
-      expect(described_class.cipher).to be_nil
-
-      ActiveRecordEncryption.with_cipher(cipher) do
-        expect(described_class.cipher).to eq(cipher)
+  describe 'ClassMethods' do
+    describe '.default_encryption/.default_encryption=' do
+      around do |example|
+        original = described_class.default_encryption
+        example.run
+        described_class.default_encryption = original
       end
 
-      expect(described_class.cipher).to be_nil
+      it 'set default encryption' do
+        new_encryption = { encryptor: :active_support, key: 'xxx', salt: 'xxx' }
+        described_class.default_encryption = new_encryption
+        expect(described_class.default_encryption).to eq(new_encryption)
+      end
     end
   end
 end
